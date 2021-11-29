fe6ae4a848b1edf783f525fa997d16d5acac8748
Ruberson <Ruberson.as@gmail.com>
2021-11-29 20:47:21 -0300

    commit 1

---
+++ app.js
@@ -0,0 +1,179 @@
+/* Imports */
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const app = express();
+
+//Config Json response
+app.use(express.json())
+
+//Models
+const User = require('./models/User')
+const { exists } = require('./models/User')
+
+
+//Open Route - Public
+app.get('/',(req, res) => {
+    res.status(200).json({msg: "Bem vindo a sua API"})
+})
+
+// Private Route
+app.get('/user/:id', checkToken ,async (req, res) => {
+
+    const id = req.params.id
+
+    // check use exists
+
+    const user = await User.findById(id,'-password')
+
+    if (!user) {
+        return res.status(404).json({msg: "Usuario nao encontrado!"})
+    }
+
+    res.status(200).json({ user })
+
+})
+
+function checkToken(req, res, next) {
+
+    const autoHeader = req.headers['authorization']
+    const token = autoHeader && autoHeader.split(" ")[1]
+
+    if (!token) {
+        return res.status(401).json({msg:'acesso negado'})
+    }
+
+    try {
+        const secret = process.env.SECRET 
+        jwt.verify(token, secret)
+
+        next()
+    } catch(error) {
+        res.status(400).json({msg: 'Token invalido'})
+    }
+}
+
+
+//Register User
+app.post('/auto/register', async(req, res) => {
+
+    const {name , email, password, confirmpassword} = req.body
+
+    // validations
+    if(!name) {
+        return res.status(422).json({msg: 'O nome é obrigatório'})
+    }
+
+    if(!email) {
+        return res.status(422).json({msg: 'O email é obrigatório'})
+    }
+
+    if(!password) {
+        return res.status(422).json({msg: 'O password é obrigatório'})
+    }
+
+    if (password !== confirmpassword) {
+        return res.status(422).json({msg: 'As Senhas não conferem'})
+    }
+
+    //check if user exists
+    const userExisits = await User.findOne({ email: email})
+
+    if (userExisits) {
+
+        return res.status(422).json({msg: 'Já existe um usuário usando este email'})
+
+    }
+
+    // create password
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // create user 
+    const user = new User({
+        name,
+        email,
+        password: passwordHash,
+    
+    })
+
+    try {
+        await user.save()
+
+        res.status(201).json({msg:"Usuario criado com sucesso"})
+
+    } catch(error) {
+
+        res.status(500).json({msg: "Erro de servidor"})
+    }
+
+
+})
+
+// login user
+app.post("/auto/login", async (req, res) => {
+
+    const { email, password } = req.body
+   
+    if(!email) {
+        return res.status(422).json({msg: 'O email é obrigatório'})
+    }
+
+    if(!password) {
+        return res.status(422).json({msg: 'O password é obrigatório'})
+    }
+
+    //check if user exists
+
+    const user = await User.findOne({ email: email})
+
+    if (!user) {
+
+        return res.status(404).json({msg: 'Usuario nao encontrado'})
+
+    }
+
+    //check if password match
+
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if (!checkPassword) {
+        return res.status(422).json({msg: 'Senha Invalida'})
+    }
+
+    try {
+
+        const secret = process.env.SECRET 
+
+        const token = jwt.sign({
+            id: user._id,
+        },
+        secret,
+        )
+
+        res.status(200).json({msg: 'Sucesso na autenticação',token})
+
+    } catch(err) {
+        console.log(error)
+        res.status(500).json({msg: "Erro de servidor"})
+    }
+
+})
+
+//Credencials
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+mongoose
+.connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.ey1gb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+.then(() => {
+    app.listen(3000)
+    console.log('Conectou ao banco')
+
+})
+.catch((err) => console.log(err))
+
